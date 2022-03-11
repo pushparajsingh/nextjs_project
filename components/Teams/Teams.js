fbf95b41374cbdea887a9630f6ce9dab863fcd15
husain <husainyuvasoft248@gmail.com>
2022-03-11 10:55:01 +0530

    teams page design

---
+++ components/Teams/Teams.js
@@ -0,0 +1,145 @@
+import Head from 'next/head'
+import {
+    Container,
+    Row,
+    Col
+} from 'react-bootstrap'
+import styles from './Teams.module.css'
+import Title from '../common/Titles/Titles'
+import Image from 'next/image'
+import TeamMember1 from '../../assets/images/teams/team-1.jpg'
+import HiteshKanwar from '../../assets/images/teams/hitesh-kanwar.png'
+import { ourTeams }	from '../Utils/ourTeams';
+
+const Teams = () => {
+	console.log(1111,ourTeams)
+
+    return(
+        <div>
+            <section className={styles.TeamHeadBg}>
+                <Container>
+					<Row className='align-items-center'>
+						<Col md={6}>
+							<Image
+								src={HiteshKanwar}
+								alt="Member image"
+								className='img-fluid'
+							/>
+						</Col>
+						<Col md={6}>
+							<div>
+								<Title />
+								<p className={styles.teamHeadDescription}>
+									Our clients’ interests are ours as well. We strive to deliver valuable products and services for both our clients and their customers. We think that great things can only be achieve through a sustainable, strong and professional team. We love challenges and we are keen learners. We measure our success by the success of those we serve. That’s not some tagline, It’s a tenet that informs everything we believe and do. As CEO of Yuvasoft, Mr. Hitesh Gurjar is responsible for technical leadership and innovation, further developing the company’s technical community, and aligning its software strategy, architecture and partner relationships to deliver customer value. He has a passion for innovation and development. He has extensive experience leading the development of cutting-edge products and technologies.
+								</p>
+								<ul className={styles.teamHeadList}>
+									<li>
+										<span>Post:</span>
+										Founder, CEO
+									</li>
+									<li>
+										<span>Experience:</span>
+										Over 6+ years experience
+									</li>
+									<li>
+										<span>Qualification:</span>
+										Bachelor of Engineering
+									</li>
+								</ul>
+								<div className={styles.teamSocial}>
+									<a>
+										<i>Facebook</i>
+										<i>Linkedin</i>
+										<i>Twitter</i>
+									</a>
+								</div>
+							</div>
+						</Col>
+					</Row>
+				</Container>
+            </section>
+            <section className={styles.teamsBgWrapper}>
+                <Container>
+                    <Row className='justify-content-center text-center'>
+                        <Col sm={6}>
+                            <Title />
+                        </Col>
+                    </Row>
+                    <Row>
+						{ourTeams && ourTeams.map((value, index)=>{
+							return (
+								<Col md={4} key={index}>
+									<div className={styles.TeamCard}>
+										<div className={styles.TeamPic}>
+											<Image
+												src={HiteshKanwar}
+												alt={value.thumb}
+												className='img-fluid'
+												// layout='fill'
+											/>
+										</div>
+										<div className={styles.TeamInfo}>
+											<h4 className={styles.TeamName}>{value.name}</h4>
+											<p className={styles.TeamPost}>{value.post}</p>
+										</div>
+									</div>
+								</Col>
+							)
+						})}
+						{/* <Col md={4}>
+							<div className={styles.TeamCard}>
+								<div className={styles.TeamPic}>
+									<Image
+										src={TeamMember1}
+										alt="Team"
+										className='img-fluid'
+									/>
+								</div>
+								<div className={styles.TeamInfo}>
+									<h4 className={styles.TeamName}>Test User</h4>
+									<p className={styles.TeamPost}>Test designation</p>
+									<div className={styles.TeamSocial}>
+										<a href='#'>
+											
+										</a>
+									</div>
+								</div>
+							</div>
+						</Col>
+						<Col md={4}>
+							<div className={styles.TeamCard}>
+								<div className={styles.TeamPic}>
+									<Image
+										src={TeamMember1}
+										alt="Team"
+										className='img-fluid'
+									/>
+								</div>
+								<div className={styles.TeamInfo}>
+									<h4 className={styles.TeamName}>Test User</h4>
+									<p className={styles.TeamPost}>Test designation</p>
+								</div>
+							</div>
+						</Col>
+						<Col md={4}>
+							<div className={styles.TeamCard}>
+								<div className={styles.TeamPic}>
+									<Image
+										src={TeamMember1}
+										alt="Team"
+										className='img-fluid'
+									/>
+								</div>
+								<div className={styles.TeamInfo}>
+									<h4 className={styles.TeamName}>Test User</h4>
+									<p className={styles.TeamPost}>Test designation</p>
+								</div>
+							</div>
+						</Col> */}
+                    </Row>
+                </Container>
+            </section>
+        </div>
+    )
+}
+export default Teams;
---
+++ components/Utils/ourTeams.js
@@ -0,0 +1,52 @@
+import Ruby from '../../assets/images/Ruby-on-rails.svg';
+
+export const ourTeams = [
+    {
+        "id": 1,
+        "name": "Prateek Singh",
+        "post": "Senior Software Engineer (ROR)",
+        "thumb": "Prateek"
+    },
+    {
+        "id": 2,
+        "name": "Mahadev Patidar",
+        "post": "Team Lead",
+        "thumb": "Mahadev"
+    },
+    {
+        "id": 3,
+        "name": "Vijay Verma",
+        "post": "Team Lead",
+        "thumb": "Vijay"
+    },
+    {
+        "id": 4,
+        "name": "Pankaj Raj Gupta",
+        "post": "Team Lead",
+        "thumb": "Pankaj"
+    },
+    {
+        "id": 5,
+        "name": "Anand Rajput",
+        "post": "Team Lead",
+        "thumb": "Anand"
+    },
+    {
+        "id": 6,
+        "name": "Vinod Dhakar",
+        "post": "Senior Software Engineer (ROR)",
+        "thumb": "Anand"
+    },
+    {
+        "id": 7,
+        "name": "Kratika Saxena",
+        "post": "Business Development Manager",
+        "thumb": "Kratika"
+    },
+    {
+        "id": 8,
+        "name": "Mitali Shastry",
+        "post": "Business Development Manager",
+        "thumb": "Mitali"
+    },
+]
