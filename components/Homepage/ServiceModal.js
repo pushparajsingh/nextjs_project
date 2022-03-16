5339264ffbc8ff0cde28d5b84c4c85cb845b2c57
aayush <aayushyuvasoft340@gmail.com>
2022-03-16 10:32:03 +0530

    fixed issue

--- components/Homepage/ServiceModal.js
+++ components/Homepage/ServiceModal.js
@@ -12,7 +12,7 @@ const ServiceModal = (props) => {
                 <Modal.Header closeButton>
                     <Modal.Title>Modal heading</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
