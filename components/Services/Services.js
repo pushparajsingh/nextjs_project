96ddcf0c3db4c5738d49a3a0f1e2ff023143b7ce
husain <husainyuvasoft248@gmail.com>
2022-03-28 18:53:34 +0530

    service page design started

--- components/Services/Services.js
+++ components/Services/Services.js
@@ -1,7 +1,47 @@
+import {
+    Container,
+    Row,
+    Col,
+    Button
+} from 'react-bootstrap';
+import Title from '../common/Titles/Titles';
+import { BsFillArrowRightCircleFill } from 'react-icons/Bs'
+import styles from './Services.module.scss'
+import Link from 'next/link'
+import ServiceIcon from "../Utils/ourServices";
+import portfolioData from "../Utils/ourPortfolio";
+
+
 const ServicesPage = () => {
     return (
-        <section>
-            ServicesPage
+        <section className={styles.serviceWrapper}>
+            <Container className={styles.serviceHead}>
+                <Row className='justify-content-between align-items-end'>
+                    <Col md={5}>
+                        <Title value="Our Services" span="Some of" subTitle="Our Services & Skills" />
+                    </Col>
+                    <Col md={6}>
+                        <Button className="themeButton">
+                            Scroll more
+                            <BsFillArrowRightCircleFill className={styles.icon} />
+                        </Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12}>
+                        <ul className={styles.serviceCategoryList}>
+                            <li key={index}>
+                                <Link href="#">
+                                    <a>
+
+                                        <h4 className={styles.serviceTitle}>Ruby On Rails</h4>
+                                    </a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
         </section>
     )
 }
