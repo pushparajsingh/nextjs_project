da13df28d65470166c87be9114d0aa816278533c
gulshantech <gulshan.rajput@techment.com>
2022-04-29 20:59:43 +0530

    fixed issue

--- pages/homepage/index.js
+++ pages/homepage/index.js
@@ -1,16 +1,20 @@
 import Homepage from "../../components/Homepage/Homepage";
 import Header from "../../components/common/Header/Header";
-import Footer from "../../components/common/Footer/Footer"
+import Footer from "../../components/common/Footer/Footer";
+import dynamic from "next/dynamic";
+
+const DynamicComponent = dynamic(() =>
+  import("../../components/Homepage/Homepage")
+);
 
 const HomepageRoute = () => {
-    return (
-        <>
-            <Header />
-            <Homepage />
-            <Footer />
-        </>
-
-    )
-}
+  return (
+    <>
+      <Header />
+      <DynamicComponent />
+      <Footer />
+    </>
+  );
+};
 
 export default HomepageRoute;
